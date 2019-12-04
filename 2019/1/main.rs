d0ac1d20dcc630a4557be3824da3ffac9f2d31d3
bobsayshilol <bobsayshilol@live.co.uk>
2019-12-04 01:28:07 +0000

    Add solutions to 2019's day 1 and 2.

---
+++ 2019/1/main.rs
@@ -0,0 +1,124 @@
+/**
+ * Part 1:
+ *
+ * Fuel required to launch a given module is based on its mass. Specifically,
+ * to find the fuel required for a module, take its mass, divide by three,
+ * round down, and subtract 2.
+ *
+ * For example:
+ *
+ *    For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to
+ *    get 2.
+ *    For a mass of 14, dividing by 3 and rounding down still yields 4, so the
+ *    fuel required is also 2.
+ *    For a mass of 1969, the fuel required is 654.
+ *    For a mass of 100756, the fuel required is 33583.
+ *
+ * The Fuel Counter-Upper needs to know the total fuel requirement. To find it,
+ * individually calculate the fuel needed for the mass of each module (your
+ * puzzle input), then add together all the fuel values.
+ *
+ *
+ * Part 2:
+ *
+ * Fuel itself requires fuel just like a module - take its mass, divide by
+ * three, round down, and subtract 2. However, that fuel also requires fuel,
+ * and that fuel requires fuel, and so on. Any mass that would require negative
+ * fuel should instead be treated as if it requires zero fuel; the remaining
+ * mass, if any, is instead handled by wishing really hard, which has no mass
+ * and is outside the scope of this calculation.
+ *
+ * So, for each module mass, calculate its fuel and add it to the total. Then,
+ * treat the fuel amount you just calculated as the input mass and repeat the
+ * process, continuing until a fuel requirement is zero or negative.
+ */
+
+
+use std::error::Error;
+
+
+// Load the inputs into a vector
+fn load_inputs() -> Vec<u64>
+{
+	// Load the inputs from file
+	let input = match std::fs::read_to_string("inputs.txt")
+	{
+		Err(error) => panic!("Failed to open inputs: {}", error.description()),
+		Ok(string) => string,
+	};
+	
+	// Go through the inputs line by line and transform them into u64s
+	let mut output = Vec::new();
+	for line in input.lines()
+	{
+		output.push(line.parse().unwrap());
+	}
+	
+	return output;
+}
+
+
+// Get the total fuel required for a list of masses
+fn get_total_fuel<Func>(masses :&Vec<u64>, calculate_fuel :&Func) -> u64
+	where Func :Fn(u64) -> u64
+{
+	// Go through the inputs one by one and accumulate the results
+	let mut total_fuel :u64 = 0;
+	for mass in masses
+	{
+		total_fuel += calculate_fuel(*mass);
+	}
+	
+	return total_fuel;
+}
+
+
+// Simple fuel calculation as described above
+fn calculate_fuel_simple(mass :u64) -> u64
+{
+	let fuel = mass / 3;
+	// Don't let it go below 0
+	return std::cmp::max(fuel, 2) - 2;
+}
+
+
+fn get_total_part1() -> u64
+{
+	// Read in the masses and calculate the total fuel required for it
+	let masses = load_inputs();
+	return get_total_fuel(&masses, &calculate_fuel_simple);
+}
+
+
+fn get_total_part2() -> u64
+{
+	// Read in the masses
+	let masses = load_inputs();
+	
+	// Slightly more complex sum, as described above
+	let calculate_fuel_complex = |mass|
+	{
+		// Nothing fancy, just keep looping until there's no changes in the
+		// amount of fuel required.
+		let mut next_fuel = calculate_fuel_simple(mass);
+		let mut total_fuel = next_fuel;
+		while next_fuel != 0
+		{
+			// Recalculate it for the smaller amount of fuel
+			next_fuel = calculate_fuel_simple(next_fuel);
+			total_fuel += next_fuel;
+		}
+		
+		return total_fuel;
+	};
+	
+	return get_total_fuel(&masses, &calculate_fuel_complex);
+}
+
+
+fn main()
+{
+	let part1 = get_total_part1();
+	let part2 = get_total_part2();
+	println!("Part1: {}\nPart2: {}", part1, part2);
+}
---
+++ 2019/2/main.rs
@@ -0,0 +1,188 @@
+/**
+ * Part 1:
+ *
+ * An Intcode program is a list of integers separated by commas (like 1,0,0,3,
+ * 99). To run one, start by looking at the first integer (called position 0).
+ * Here, you will find an opcode - either 1, 2, or 99. The opcode indicates
+ * what to do; for example, 99 means that the program is finished and should
+ * immediately halt. Encountering an unknown opcode means something went wrong.
+ *
+ * Opcode 1 adds together numbers read from two positions and stores the result
+ * in a third position. The three integers immediately after the opcode tell
+ * you these three positions - the first two indicate the positions from which
+ * you should read the input values, and the third indicates the position at
+ * which the output should be stored.
+ *
+ * For example, if your Intcode computer encounters 1,10,20,30, it should read
+ * the values at positions 10 and 20, add those values, and then overwrite the
+ * value at position 30 with their sum.
+ *
+ * Opcode 2 works exactly like opcode 1, except it multiplies the two inputs
+ * instead of adding them. Again, the three integers after the opcode indicate
+ * where the inputs and outputs are, not their values.
+ *
+ * Once you're done processing an opcode, move to the next one by stepping
+ * forward 4 positions.
+ *
+ *
+ * Part 2:
+ *
+ * The inputs should still be provided to the program by replacing the values
+ * at addresses 1 and 2, just like before. In this program, the value placed in
+ * address 1 is called the noun, and the value placed in address 2 is called
+ * the verb. Each of the two input values will be between 0 and 99, inclusive.
+ *
+ * Once the program has halted, its output is available at address 0, also just
+ * like before. Each time you try a pair of inputs, make sure you first reset
+ * the computer's memory to the values in the program (your puzzle input) - in
+ * other words, don't reuse memory from a previous attempt.
+ *
+ * Find the input noun and verb that cause the program to produce the output
+ * 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2,
+ * the answer would be 1202.)
+ */
+
+
+use std::error::Error;
+
+
+// Load the input into a buffer to be treated as Intcode RAM
+fn load_program() -> Vec<usize>
+{
+	// Load the input from file
+	let input = match std::fs::read_to_string("input.txt")
+	{
+		Err(error) => panic!("Failed to open input: {}", error.description()),
+		Ok(string) => string,
+	};
+	
+	// Transform the elements into integers
+	let output :Vec<usize> = input
+		.trim() // ignore trailing whitespace
+		.split(',') // split on ','
+		.map(|val| val.parse().unwrap()) // parse each value into a usize
+		.collect(); // combine into a Vec
+	
+	return output;
+}
+
+
+// Pretty print a program (but not that pretty)
+#[allow(dead_code)]
+fn dump_program(program :&Vec<usize>)
+{
+	println!("Program:");
+	for idx in 0..program.len()
+	{
+		print!("{},\t", program[idx]);
+		if (idx & 7) == 7
+		{
+			println!();
+		}
+	}
+	println!();
+}
+
+
+// Runs a prepared program
+fn run_program(program :&mut Vec<usize>)
+{
+	// Program counter starts at 0
+	let mut pc = 0;
+	loop
+	{
+		// Read off the next instruction
+		let ins = program[pc];
+		
+		// Handle the instruction
+		match ins
+		{
+			1 => {
+				// Load the sources and dest
+				let s0 = program[pc + 1];
+				let s1 = program[pc + 2];
+				let d0 = program[pc + 3];
+				
+				// Do the op
+				program[d0] = program[s0] + program[s1];
+			},
+			
+			2 => {
+				// Load the sources and dest
+				let s0 = program[pc + 1];
+				let s1 = program[pc + 2];
+				let d0 = program[pc + 3];
+				
+				// Do the op
+				program[d0] = program[s0] * program[s1];
+			},
+			
+			// Break out
+			99 => break,
+			
+			// This shouldn't happen
+			_ => panic!("Unknown ins: {}", ins),
+		}
+		
+		// Increment to the next ins
+		pc += 4;
+	}
+}
+
+
+fn run_part1() -> usize
+{
+	// Load the program as provided
+	let mut program = load_program();
+	
+	// Modify it as required
+	program[1] = 12;
+	program[2] = 2;
+	
+	// Run it
+	run_program(&mut program);
+	
+	// Return the first value
+	return program[0];
+}
+
+
+fn main()
+{
+	println!("Part1 = {}", run_part1());
+	println!("Part2 = {}", run_part2());
+}
+
+
+fn run_part2() -> usize
+{
+	// Load the program as provided
+	let program_clean = load_program();
+	
+	// Brute force it
+	for noun in 0..99
+	{
+		for verb in 0..99
+		{
+			// Make a copy of the program
+			let mut program = program_clean.clone();
+			
+			// Mutate the program
+			program[1] = noun;
+			program[2] = verb;
+			
+			// Run it
+			run_program(&mut program);
+			
+			// See if that was what we wanted
+			if program[0] == 19690720
+			{
+				// Return the combined value
+				return noun * 100 + verb;
+			}
+		}
+	}
+	
+	panic!("Didn't find it?");
+}
+
